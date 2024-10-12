2e0a949f636f5c4672bd13ebfe4fc8861f93472e
EnzoButori <enzobutoric@gmail.com>
2024-10-12 18:05:46 -0300

    Avance 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
+/* Menu de interaccion con el usuario por consola*/
 
+function gracias(){
+    alert("Gracias por su compra, vuelva pronto!")
+}
+
+function productos(){
+    console.log("")
+}
+
+function menu () {
+    console.log("\tMenu de Opciones.\n1. Productos Disponibles.\n2. Ver Lista de precios.\n3. Plan Canje.\n4. Realizar Compra.\n0. Finalizar.")
+    let numero = parseInt(prompt("Seleccione una opcion"))
+
+    switch (numero){
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 0:
+            gracias()
+    }
+
+}
+
+menu()
